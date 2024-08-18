0c31d7972dfb52a30dd588d01a3e29470c827fe9
DripTrace <rpalm@driptrace.io>
2024-08-18 16:20:28 -0700

    migrate

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
-# build_files.sh
+#!/usr/bin/env bash
 
 pip3 install -r requirements.txt
 python3.12 src/manage.py collectstatic --noinput
-# python3.12 src/manage.py runserver
+python3.12 src/manage.py makemigrations --noinput
+python3.12 src/manage.py migrate --noinput
