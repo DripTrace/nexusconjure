75464b912057fef06e46a018512d40fc9a752f07
DripTrace <rpalm@driptrace.io>
2024-08-18 16:03:35 -0700

    more static mess

--- src/rpalmdata/urls.py
+++ src/rpalmdata/urls.py
@@ -1,36 +1,53 @@
-"""
-URL configuration for rpalmdata project.
+# """
+# URL configuration for rpalmdata project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# The `urlpatterns` list routes URLs to views. For more information please see:
+#     https://docs.djangoproject.com/en/5.0/topics/http/urls/
+# Examples:
+# Function views
+#     1. Add an import:  from my_app import views
+#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
+# Class-based views
+#     1. Add an import:  from other_app.views import Home
+#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+# Including another URLconf
+#     1. Import the include() function: from django.urls import include, path
+#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+# """
+# from django.contrib import admin
+# from django.urls import path
+# # from django import static
+# # from django import settings
+# from .settings import STATIC_URL
+# from .settings import STATIC_ROOT
+# from .settings import DEBUG
+
+# from .api import api
+# # from .settings import settings
+
+# urlpatterns = [
+#     path("admin/", admin.site.urls),
+#     path("api/", api.urls)
+# ]
+
+# # urlpatterns = [
+# #     path("admin/", admin.site.urls),
+# # ]
+
+# if DEBUG:
+#     urlpatterns += static(STATIC_URL, document_root=STATIC_ROOT)
+
 from django.contrib import admin
 from django.urls import path
-# from django import static
-# from django import settings
-# from .settings import settings
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .api import api
-# from .settings import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", api.urls)
 ]
-
-# urlpatterns = [
-#     path("admin/", admin.site.urls),
-# ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
