f57e102ec3e163d1fe46dea0843faa24542b905c
DripTrace <rpalm@driptrace.io>
2024-08-18 16:22:50 -0700

    col

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 pip3 install -r requirements.txt
-python3.12 src/manage.py collectstatic --noinput
 python3.12 src/manage.py makemigrations --noinput
 python3.12 src/manage.py migrate --noinput
+python3.12 src/manage.py collectstatic --noinput
