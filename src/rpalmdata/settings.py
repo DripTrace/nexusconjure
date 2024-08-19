60640543844ba6b99a39249854785b29d0b610f8
DripTrace <rpalm@driptrace.io>
2024-08-18 18:17:22 -0700

    static

--- src/rpalmdata/settings.py
+++ src/rpalmdata/settings.py
@@ -188,8 +188,9 @@ NINJA_JWT = {
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "rpalmdata", "static")]
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
-STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
 
 # STATIC_ROOT = os.path.join(BASE_DIR, “staticfiles_build”, “static”)
