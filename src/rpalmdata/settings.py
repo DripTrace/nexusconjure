4434f667a766c9d66e51db33f96b2908c167a40a
DripTrace <rpalm@driptrace.io>
2024-08-18 18:26:02 -0700

    s

--- src/rpalmdata/settings.py
+++ src/rpalmdata/settings.py
@@ -13,9 +13,9 @@ import os
 import datetime
 from pathlib import Path
 from decouple import config
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
