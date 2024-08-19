ffd12d8aa87801d28f344e2649141d53e7284bc1
DripTrace <rpalm@driptrace.io>
2024-08-18 18:58:00 -0700

    jjj

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-pip3 install -r requirements.txt
+# pip3 install -r requirements.txt
+# python3.12 src/manage.py makemigrations --noinput
+# python3.12 src/manage.py migrate --noinput
+# python3.12 src/manage.py collectstatic --noinput --clear
+
+echo "Installing the latest version of pip..."
+python3.12 -m pip3 install --upgrade pip
+
+# Build the project
+echo "Building the project..."
+python3.12 -m pip3 install -r requirements.txt
+
 python3.12 src/manage.py makemigrations --noinput
 python3.12 src/manage.py migrate --noinput
+
+# Collect static files
+echo "Collecting static files..."
 python3.12 src/manage.py collectstatic --noinput --clear
