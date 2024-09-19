8b277956d908c591c2fdfb3caff27b1a66ee3648
DripTrace <rpalm@driptrace.io>
2024-09-18 18:43:32 -0700

    change

--- build_files.sh
+++ build_files.sh
@@ -12,9 +12,9 @@ python3.12 -m pip install --upgrade pip
 echo "Building the project..."
 pip3 install -r requirements.txt
 
-python3.12 src/manage.py makemigrations --noinput
-python3.12 src/manage.py migrate --noinput
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
 
 # Collect static files
 echo "Collecting static files..."
-python3.12 src/manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear
